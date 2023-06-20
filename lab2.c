072c0577625b4db4d6f998fa6bd688b42cb58203
franky <franco.cornejo.g@usach.cl>
2023-06-20 00:52:28 -0400

    volcado de datos del archivo en arreglo

--- lab2.c
+++ lab2.c
@@ -1,46 +1,67 @@
-#include <stdio.h>
-#include <string.h>
-//#include <regex.h>
-#include <unistd.h>
+//-----LABORATORIO 2 -------//
+//ARCHIVO PRINCIPAL PARA EL PROGRAMA
+/*RAMO: SISTEMAS OPERATIVOS*/
+/*AUTORES: FELIPE CORNEJO - CARLOS RETAMALES*/
+/*AYUDANTE: BENJANMIN MUÑOZ*/
 
-#include "funciones.h"
+#include <ctype.h>  //getopt
+#include <stdio.h>  //entrada y salida
+#include <stdlib.h> //memoria dinamica
+#include <string.h> //manejo de string
+#include <unistd.h> //Uso de fork(), pipes(), entre otros
+#include <sys/wait.h> //Constantes simbólicas (waitpid(), wait(), etc)
+#include <sys/types.h> //Uso de tipos de datos ( pid_t )
+#include "funciones.h" //Funciones para volcar archivo en lista
 
-/* Para usar este programa ./a.out -i prueba_1000.txt -o salida.txt */
+//Definicion para las Pipes
+#define LECTURA 0
+#define ESCRITURA 1
 
 
-int main(int argc, char *argv[]) {
-    int opt;
-    char *nombre_archivo_entrada = NULL;
-    char *nombre_archivo_salida = NULL;
-    int chunks = 0;
-    int workers = 0;
-    int mostrar_datos = 0;
-    
-    // gestion de parametros de entrada
-    while((opt = getopt(argc, argv, "i:o:bc:n:")) != -1) {
-        switch(opt){
-            case 'i':
-                nombre_archivo_entrada = optarg;
-                break;
-            case 'o':
-                nombre_archivo_salida = optarg;
-                break;
-            case 'b':
-                mostrar_datos ++;
-                break;
-            case 'c':
-                chunks = atoi(optarg);
-                break;
-            case 'n':
-                workers = atoi(optarg);
-                break;
-            default:
-                fprintf(stderr, "Uso: %s [-i inputfile] [-o outputfile] [-b] [-c chunks] [-n workers]\n", argv[0]);
-                return 1;
-        }
+int main (int argc, char **argv)
+{
+    /*
+    _____________________
+    Entrada por terminal
+    ---------------------
+    -i -o -n -c -b
+    -i filename : archivo con el bombardeo
+    -o filename : archivo de salida
+    -n: cantidad de workers a generar.
+    -c: tamaño del chunk, es decir, cantidad de líneas a leer por chunk.
+    -b: bandera o flag que permite indicar si se quiere ver por consola la cantidad de expresiones regulares encontradas por cada proceso worker.
+    */
+
+    int in, n = 0, c = 0, b = 0;
+    char * filename_in = NULL;
+    char *filename_out = NULL;
+
+    while ((in = getopt (argc, argv, "i:o:n:c:b")) != -1)
+    switch (in)
+    { 
+        case 'n': //Numero de workers
+            sscanf(optarg, "%d", &n);
+            break;
+        case 'c': //tamaño del chunks
+            sscanf(optarg, "%d", &c);
+            break;
+        case 'b': //bandera
+            sscanf(optarg, "%d", &c);
+            break;
+        case 'i': //Archivo entrada
+            filename_in = optarg;
+            break;
+        case 'o': //Archivo de salida
+            filename_out = optarg;
+            break;
     }
-}
-    
+
+    if(verificar(n,p,c,filename_in))
+    {
+      printf("ERROR: variable no valida");
+      exit(0);
+    }
+
     int cantidad_lineas_archivo = contadorLineas(nombre_archivo_entrada);
     char * lista_string[cantidad_lineas_archivo + 3];
     volcarArchivo(nombre_archivo_entrada, lista_string, cantidad_lineas_archivo);
@@ -49,8 +70,16 @@ int main(int argc, char *argv[]) {
     int total_expre_no = 0;
 
 
-    /* --------Aqui codigo verificacion-----------------*/
+    //Inicializamos para comunicarnos con los hijos 
+    int pid;
 
-    return 0;
+    //Se forman las pipes
+    int** pipes = (int**)malloc(sizeof(int*)*p); 
+
+    //Definicion de pipes siendo esto una matriz que [i][j]
+    for(int i = 0; i<p;i++){ 
+        pipes[i] = (int*)malloc(sizeof(int)*2);
+
+    }
+
 }
-
